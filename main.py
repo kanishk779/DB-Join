aa7e0c5056bb2098c5cd820943e5d53722f7c1a4
kanishk779 <kashyapamankumar54@gmail.com>
2021-03-15 14:16:18 +0530

    Add join right method

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ class MergeJoin:
         self.right_sublist = 0
         self.left_memory = []  # stores the left sorted sublist first blocks
         self.right_memory = []  # stores the right sorted sublist first blocks
-        self.buffer = []  # one extra buffer for output purpose
+        self.buffer = []  # one extra buffer for output purpose, list of strings
         self.left_sublist_offsets = []
         self.right_sublist_offsets = []
         self.left_file_list = []
         self.right_file_list = []
+        self.left_sublist_read = []  # how much data we have actually read from each left sublist
+        self.right_sublist_read = []  # how much data we have actually read from each right sublist
         self.find_tuple_size()
 
     def find_tuple_size(self):
@@ -45,6 +47,7 @@ class MergeJoin:
         print('files to be created : {a}'.format(a=files_to_be_created))
         self.left_sublist = files_to_be_created
         self.left_sublist_offsets = [0] * files_to_be_created  # initially all the sublist start from first block
+        self.left_sublist_read = [0] * files_to_be_created
         left = open(self.left_relation, 'r')
         for i in range(files_to_be_created):
             arr = left.readlines(read_till-1)  # need to subtract one otherwise one more line will be read
@@ -65,6 +68,7 @@ class MergeJoin:
         right_file_size = os.stat(self.right_relation).st_size
         files_to_be_created = math.ceil(right_file_size / read_till)
         self.right_sublist_offsets = [0] * files_to_be_created  # initially all the sublist start from first block
+        self.right_sublist_read = [0] * files_to_be_created
         self.right_sublist = files_to_be_created
         right = open(self.right_relation, 'r')
         for i in range(files_to_be_created):
@@ -102,6 +106,7 @@ class MergeJoin:
         # find the minimum from the above
         left_not_processed = [1] * self.left_sublist
         right_not_processed = [1] * self.right_sublist
+        xx = ""
         while any(left_not_processed) and any(right_not_processed):
             pass
         left_min = "~"  # since it is the largest character
@@ -123,6 +128,7 @@ class MergeJoin:
             x, y = temp.split(' ')
             if y < left_min:
                 left_min = y
+                xx = x
         right_min = "~"
         for i in range(self.right_sublist):
             if right_not_processed[i] == 0:
@@ -168,11 +174,47 @@ class MergeJoin:
                     else:
                         break
         else:
-            pass
+            # the left min will join with all the right ones
+            self.join_right(xx, left_min)
+            
 
         # collect all the tuples with Y = the above found minimum
 
-        # use O(N^2) for producing all the tuples and stores
+    def join_right(self, xx, left_y):
+        """
+        join x, y with the right table
+        :return: nothing
+        """
+        files = []
+        for i in range(self.right_sublist):
+            a = open(self.right_relation + str(i), 'r')
+            a.seek(self.right_sublist_read[i])
+            files.append(a)
+        # needs to maintain the pointers for right file as well
+        for i in range(self.right_sublist):
+            ind = self.right_sublist_offsets[i]
+            temp_mem = self.right_memory[i]
+            temp = temp_mem[ind]
+            y, z = temp.split(' ')
+
+            while y == left_y:
+                to_write = xx + ' ' + y + ' ' + z
+                self.buffer.append(to_write)
+                if len(self.buffer) == self.tuples:
+                    self.write_out()
+                ind += 1
+                if ind == len(temp_mem):
+                    # need to re fill the block
+                    to_read = self.tuples * self.right_tuple_size
+                    arr = files[i].readlines(to_read - 1)
+                    if len(arr) == 0:
+                        break
+                    else:
+                        temp_mem = arr
+                    ind = 0
+                temp = temp_mem[ind]
+                y, z = temp.split(' ')
+
 
 
 if __name__ == '__main__':
