2dc4b39858ad11771640b192df12413f65d403b5
kanishk779 <kashyapamankumar54@gmail.com>
2021-03-20 18:51:21 +0530

    Add to Readme

--- 2019121010.sh
+++ 2019121010.sh
@@ -2,7 +2,7 @@
 g++ -std=c++11 checker.cpp
 ./a.out $1 $2 outout.txt
 python3 main.py $1 $2 $3 $4
-#comm -3 <(sort -u outout.txt) <(sort -u inputR_inputS_join.txt)
+comm -3 <(sort -u outout.txt) <(sort -u inputR_inputS_join.txt)
 rm outout.txt
 rm $(ls | grep -E "inputR[0-9]+")
 rm $(ls | grep -E "inputS[0-9]+")
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sturdy-spoon
 Iterators for Database
+How to run ?
+Usage: java -jar JoinGenerator.jar noofrecordsinR noofrecordsinS sizeofX sizeofY sizeofZ
+
+Output:
+Two files will be generated inputR and the inputS
+column data separated by a single space.
+
